190afaa01898af61a72ec46fd0a112989a4c3928
Robert Missbach <mail@robert-missbach.com>
2014-06-15 23:25:03 +0200

    Initial Commit

---
+++ Pixelino/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pixelino
+//
+//  Created by Robert Mißbach on 15.06.14.
+//  Copyright (c) 2014 Robert Mißbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
